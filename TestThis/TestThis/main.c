7aaff310dc42f3dfa4e8ec39290a09d5445e1c19
Bob Madonna <bob.madonna@savant.com>
2017-11-24 16:33:09 -0500

    init

---
+++ TestThis/TestThis/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  TestThis
+//
+//  Created by Robert Madonna on 11/24/17.
+//  Copyright © 2017 Robert Madonna. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World from test xcode!\n");
+    return 0;
+}
